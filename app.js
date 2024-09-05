f097e2e0318361bb0f91b6654636b9076e272ce1
goushaa <ahmed.elkady@tactful.ai>
2024-09-06 00:24:54 +0300

    middlewares & refactoring

--- app.js
+++ app.js
@@ -1,68 +1,31 @@
 const express = require('express');
-const fs = require('fs');
-
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 const app = express();
 
+// 1) MIDDLEWARES
+
+app.use(morgan('dev'));
 app.use(express.json()); //add data of body
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req, res) => {
-  res
-    .status(200)
-    .json({ status: 'success', results: tours.length, data: { tours } });
-};
-
-const getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  if (!tour)
-    return res.status(404).json({ status: 'fail', message: 'Invalid id' });
-  res.status(200).json({ status: 'success', tour: tour });
-};
-
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
-    }
-  );
-};
-
-const updateTour = (req, res) => {
-  res.status(200).json({ status: 'success', data: 'updated tour here...' });
-};
-
-const deleteTour = (req, res) => {
-  res.status(204).json({ status: 'success', data: null });
-};
-
-// app.get('/api/v1/tours', getAllTours);
-// app.post('/api/v1/tours', createTour);
-// app.get('/api/v1/tours/:id', getTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
-
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`running on port ${port}...`);
+app.use((req, res, next) => {
+  console.log('new request passed in middleware :)');
+  next();
 });
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+// 2) ROUTE HANDLERS
+// inside routes directory
+
+// 3) ROUTES
+
+// inside routes directory
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app; // 4) start server
---
+++ controllers/userController.js
@@ -0,0 +1,18 @@
+exports.getAllUsers = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'this route is not yet' });
+};
+
+exports.createUser = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'this route is not yet' });
+};
+
+exports.getUser = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'this route is not yet' });
+};
+
+exports.updateUser = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'this route is not yet' });
+};
+exports.deleteUser = (req, res) => {
+  res.status(500).json({ status: 'error', message: 'this route is not yet' });
+};
