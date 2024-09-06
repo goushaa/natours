c5ad511d1421c233bcc6efcc275bf8dd35c65870
goushaa <ahmed.elkady@tactful.ai>
2024-09-06 04:15:54 +0300

    remove unnecessary console log

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-console.log(process.env.DATABASE_USER, process.env.DATABASE_PASSWORD);
-
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`running on port ${port}...`);
