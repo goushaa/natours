a61ee01b1e279224c6ba2863cec26ab0ea6b7f5b
goushaa <ahmed.elkady@tactful.ai>
2024-09-06 20:21:42 +0300

    adding dev data

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.envSecret' });
+
+const DB = process.env.DATABASE.replace(
+  '<DB_PASSWORD>',
+  process.env.DB_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('db connection successful'));
+
+// READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8'),
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('data successfully loeaded');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+// IMPORT DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('data successfully deleted');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
