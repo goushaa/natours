22f1265ffd447c0224ac2703701bc2b010a30f6b
goushaa <ahmed.elkady@tactful.ai>
2024-09-06 16:22:24 +0300

    connect to db

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+dotenv.config({ path: './.envSecret' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<DB_PASSWORD>',
+  process.env.DB_PASSWORD,
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('db connection successful'));
 
 const port = process.env.PORT;
 app.listen(port, () => {
