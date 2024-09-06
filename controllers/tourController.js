0ab43e3743eb66976b712ecc9a9df97dda76191d
goushaa <ahmed.elkady@tactful.ai>
2024-09-06 22:15:27 +0300

    filtering, sorting, limiting, paginating, and aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,26 @@
 const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find({});
+    // BUILD QUERY
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
+    // EXECUTE QUERY
+    const tours = await features.query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
