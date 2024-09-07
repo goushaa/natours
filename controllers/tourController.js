dbc4fed3ac942853312a5c357d4e18f4a8aca2ae
goushaa <ahmed.elkady@tactful.ai>
2024-09-07 19:14:48 +0300

    mongoose middlewares

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -54,7 +54,7 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: error.errmsg,
+      message: error,
     });
   }
 };
