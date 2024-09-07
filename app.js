6633d63ae227115bc9384b7e01027a4ff965f132
goushaa <ahmed.elkady@tactful.ai>
2024-09-07 21:18:37 +0300

    ndb and global error middleware

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ app.use(express.json()); //add data of body
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('new request passed in middleware :)');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -30,5 +25,22 @@ app.use((req, res, next) => {
 // inside routes directory
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app; // 4) start server
