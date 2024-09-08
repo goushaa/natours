26c689f0fc1383cdc67da5cf9d83bfb45acc995a
goushaa <ahmed.elkady@tactful.ai>
2024-09-08 04:14:35 +0300

    fix bug

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const dotenv = require('dotenv');
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION, Shutting down...');
   console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
+  process.exit(1);
 });
 
 dotenv.config({ path: './config.env' });
