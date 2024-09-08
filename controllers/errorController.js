5d182239745b3e11adb0bc9e4daef1508c154e35
goushaa <ahmed.elkady@tactful.ai>
2024-09-08 04:13:03 +0300

    handling errors

---
+++ controllers/errorController.js
@@ -0,0 +1,61 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/); // Grabbing values between quotation marks
+
+  const message = `Duplicate field value: ${value[0]}, please use another value}`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `invalid input data: ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+    // Unknown error, don't leak
+  } else {
+    console.log('ERROR', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'something went very wrong',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') sendErrorDev(err, res);
+  else if (process.env.NODE_ENV === 'production') {
+    let error = Object.create(err);
+    if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+
+    sendErrorProd(error, res);
+  }
+};
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+//sync
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION, Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
 dotenv.config({ path: './config.env' });
 dotenv.config({ path: './.envSecret' });
 
@@ -21,6 +30,15 @@ mongoose
   .then(() => console.log('db connection successful'));
 
 const port = process.env.PORT;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`running on port ${port}...`);
+});
+
+//async
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION, Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
