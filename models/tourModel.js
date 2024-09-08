432204ccce145bc1f7a9e3ba53ce0fcb40eed06f
goushaa <ahmed.elkady@tactful.ai>
2024-09-08 20:51:01 +0300

    login and signup

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -11,7 +10,6 @@ const tourSchema = new mongoose.Schema(
       trim: true,
       maxlength: [40, 'A tour name must have less or equal than 40 characters'],
       minlength: [5, 'A tour name must have more or equal than 5 characters'],
-      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
     },
     slug: String,
     duration: {
