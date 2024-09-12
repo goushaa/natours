481601276dba71d6ab11bf5e2558721ca314ea7f
goushaa <ahmed.elkady@tactful.ai>
2024-09-13 00:54:46 +0300

    get factory

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
+const APIFeatures = require('../utils/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -33,5 +34,42 @@ exports.createOne = (Model) =>
     res.status(201).json({
       status: 'success',
       data: newModel,
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({ status: 'success', data: doc });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // ALLOW NESTED GET REVIEWS ON TOUR
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    // BUILD QUERY
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    // EXECUTE QUERY
+    const docs = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: { docs },
     });
   });
--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,40 +1,7 @@
 const Review = require('../models/reviewModel');
-// const APIFeatures = require('../utils/apiFeatures');
-// const AppError = require('../utils/appError');
-const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-  //   // BUILD QUERY
-  //   const features = new APIFeatures(Review.find(), req.query)
-  //     .filter()
-  //     .sort()
-  //     .limitFields()
-  //     .paginate();
-
-  //   // EXECUTE QUERY
-  //   const reviews = await features.query;
-  const reviews = await Review.find(filter);
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: { reviews },
-  });
-});
-
-// exports.getReview = catchAsync(async (req, res, next) => {
-//   const tour = await Review.findById(req.params.id);
-
-//   if (!tour) {
-//     return next(new AppError('No tour found with that ID', 404));
-//   }
-
-//   res.status(200).json({ status: 'success', tour: tour });
-// });
+exports.getAllReviews = factory.getAll(Review);
 
 exports.setTourUserIds = (req, res, next) => {
   // ALLOW NESTED ROUTES
@@ -43,6 +10,7 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
