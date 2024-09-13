70b0629902b64e53cf738c90f3a527b18fba2b9b
goushaa <ahmed.elkady@tactful.ai>
2024-09-13 20:43:02 +0300

    setting up pug and rendering

--- app.js
+++ app.js
@@ -5,16 +5,25 @@ const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
+const path = require('path');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
+// SET PUG ENGINE
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLEWARES
+
+// SERVING STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
 
 // SET SECURITY HTTP HEADERS
 app.use(helmet());
@@ -56,9 +65,6 @@ app.use(
   }),
 );
 
-// SERVING STATIC FILES
-app.use(express.static(`${__dirname}/public`));
-
 // TESTING MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -71,6 +77,7 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 // inside routes directory
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
