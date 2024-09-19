06efe440d657e5ab9748796b1e8a57ba28918653
goushaa <ahmed.elkady@tactful.ai>
2024-09-19 15:55:09 +0300

    hehe

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
+const Stripe = require('stripe');
 
 export const bookTour = async (tourId) => {
   try {
     // You don't even need next line
     const stripe = Stripe(process.env.STRIPE_PUBLIC_KEY);
-    const res = await axios(
-      `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`,
-    );
+    const res = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
     console.log(res);
 
     if (res.status === 200) location.assign(res.data.data.url);
